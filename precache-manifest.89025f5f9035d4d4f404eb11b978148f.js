0004f011df72d5ba5bddaf9fd673bf0fdf913620
gilberto-009199 <gilberto.oliveira@aluno.faculdadeimpacta.com.br>
2021-12-20 16:44:15 +0000

    Updates

---
+++ precache-manifest.89025f5f9035d4d4f404eb11b978148f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeecff29e00f6972cbdcc527a29a5a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680fb0892e012b2b8e01",
+    "url": "/static/css/main.a244fd9e.chunk.css"
+  },
+  {
+    "revision": "18f3b74cba68f785b5e2",
+    "url": "/static/js/2.90bb61e4.chunk.js"
+  },
+  {
+    "revision": "443d2105bbae44cc4ccf3384197799b0",
+    "url": "/static/js/2.90bb61e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680fb0892e012b2b8e01",
+    "url": "/static/js/main.57ca888a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "4950ba52c6fd1bb61346db8791ca6f68",
+    "url": "/static/media/0gh2TEnl.4950ba52.png"
+  },
+  {
+    "revision": "ad7b0a94a157c56263e0a0516125561a",
+    "url": "/static/media/5k2FRkHl.ad7b0a94.png"
+  },
+  {
+    "revision": "a41f6fd8c5dfd993583b53719c79dd34",
+    "url": "/static/media/7SGW7xgl.a41f6fd8.png"
+  },
+  {
+    "revision": "0c7b1570f6239a3489dc361baa73d386",
+    "url": "/static/media/OPwuzbbl.0c7b1570.png"
+  },
+  {
+    "revision": "0794093509a3f97fa6c3dd46a089c1b4",
+    "url": "/static/media/WfA8lYal.07940935.png"
+  },
+  {
+    "revision": "71e65dd27ff18fde1787b0f868ae2ea1",
+    "url": "/static/media/avatarRostoAcenandoSemFundo.71e65dd2.png"
+  },
+  {
+    "revision": "c2f78663fed959f8c9a30191433da6af",
+    "url": "/static/media/banner-mobile.c2f78663.png"
+  },
+  {
+    "revision": "c7fdc8184a7eda37f59ef30388c81ad6",
+    "url": "/static/media/bg1.c7fdc818.jpg"
+  },
+  {
+    "revision": "d2871b5fb937ccbcef7035c357fc6965",
+    "url": "/static/media/bg6.d2871b5f.png"
+  },
+  {
+    "revision": "69b03337dabce71632cb7eddf2047204",
+    "url": "/static/media/cDAj2PSl.69b03337.png"
+  },
+  {
+    "revision": "fc599989ec600b3edb883a8e17f1d912",
+    "url": "/static/media/databases2.fc599989.jpg"
+  },
+  {
+    "revision": "2f990a83c82e2dcaee22e047dc20b4c6",
+    "url": "/static/media/globo.2f990a83.png"
+  },
+  {
+    "revision": "13bc635808105809d86ba8c4fbffadda",
+    "url": "/static/media/globo2.13bc6358.png"
+  },
+  {
+    "revision": "3948f64e118e99dabc5a01b73833ec14",
+    "url": "/static/media/logo-html6.3948f64e.png"
+  },
+  {
+    "revision": "3b614a76c0b3ad0164baf164b3582c5e",
+    "url": "/static/media/logo-nodejs.3b614a76.jpg"
+  },
+  {
+    "revision": "18c47a89fb3fbe1ee672ca65d055ce65",
+    "url": "/static/media/logo-php.18c47a89.jpeg"
+  },
+  {
+    "revision": "0940864395b44ee5fd26e4ed68750836",
+    "url": "/static/media/logo-react.09408643.png"
+  },
+  {
+    "revision": "ac44060fb79c893524a1a78fab2a98d8",
+    "url": "/static/media/logo-stackoverflow.ac44060f.png"
+  },
+  {
+    "revision": "a18d22ebbbb208f220b4a9f28ad89a05",
+    "url": "/static/media/logo-wikipedia.a18d22eb.png"
+  },
+  {
+    "revision": "a1ff5ea4b5043dee2773c80012448ac8",
+    "url": "/static/media/segoeui.a1ff5ea4.woff"
+  },
+  {
+    "revision": "f401876e0314fa9866ebf47833054c61",
+    "url": "/static/media/vsd6inZl.f401876e.png"
+  },
+  {
+    "revision": "6e54ab209df333e262b1c4cabf7c13a2",
+    "url": "/static/media/xMh0NLGl.6e54ab20.png"
+  }
+]);
